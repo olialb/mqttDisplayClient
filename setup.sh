5de5c92bce4486ca959d98729f7633e1d1f052af
olialb <olialb@posteo.de>
2024-12-14 12:26:51 +0100

    Adapt setup.sh that features can be installed with -f option
    Disable pyautogui feature by default

--- setup.sh
+++ setup.sh
@@ -12,22 +12,42 @@ trap 'abort' 0
 #exit on error
 set -e
 
+##################
+#options
+##################
+pyautogui=disabled
+
+while getopts "f:" opt; do
+	case $opt in
+		f)
+			if [ $OPTARG = pyautogui ]
+			then
+				echo "Install pyautogui!"
+				pyautogui=enabled
+			fi
+			;;
+	esac
+done
+
 echo "#########################################"
 echo "Create virtual environment"
 echo "#########################################"
 python3 -m venv venv
 source venv/bin/activate
 
-echo "#####################################################################"
-echo "Install required libs"
-echo "You can remove this section if you don't need the pyautogui feature"
-echo "#####################################################################"
-sudo apt-get update
-sudo apt-get install -y build-essential libsqlite3-dev   libpng-dev libjpeg-dev
-sudo apt-get install -y  python3-tk python3-dev
-python -m pip install --upgrade pip
-python -m pip install --upgrade Pillow
-pip install pyautogui
+if [ $pyautogui = enabled ]
+then	
+	echo "###########################################################################"
+	echo "Install required libs for pyautogui feature"
+	echo "!!Please remember to enable the feature in the mqttDisplayClient.ini file!!"
+	echo "###########################################################################"
+	sudo apt-get update
+	sudo apt-get install -y build-essential libsqlite3-dev   libpng-dev libjpeg-dev
+	sudo apt-get install -y  python3-tk python3-dev
+	python -m pip install --upgrade pip
+	python -m pip install --upgrade Pillow
+	pip install pyautogui
+fi
 
 echo "#########################################"
 echo "Install the required python packages..."
@@ -40,9 +60,10 @@ pip install validators
 pip install paho-mqtt
 
 echo "#########################################"
-echo "Fill things template"
+echo "Fill templates"
 echo "#########################################"
 echo ""
+eval "echo \"$(cat mqttDisplayClient.ini.template)\"" >mqttDisplayClient.ini
 python openhabFillThingsTemplate.py 
  
 echo "################################################"
